72867bb895e846d248c5179557ec2278aecadf19
Taguhi Abgar <taguhiabgar@gmail.com>
2025-03-09 17:46:19 +0400

    Moved homework1 requirements to a markdown file.

--- CppTemplates/CppTemplates/CppTemplates/Homework1.h
+++ CppTemplates/CppTemplates/CppTemplates/Homework1.h
@@ -7,33 +7,6 @@
 
 #pragma once
 
-/*
- # Homework 1
- 
- Write a function template, which will print content of provided
- `std::vector<>` to the console. If it is vector of integers, an example
- output is:
- ```
- [5, 32, 14, 6]
- ```
- Write its specialization for printing vector of strings, so every string will
- be double-quoted. Example output is:
- ```
- ["hello", "kitty", "move", "on"]
- ```
- Write another specialization for printing vector of characters, which will
- print every character in single quotes. Example:
- ```
- ['f', 'h', 'Z', 'X']
- ```
- Finally, write a specialization for printing vector of `std::pair<>` objects.
- In that case, each pair should appear in brackets. If the vector consists
- of pairs of strings and integers, then example output is:
- ```
- [(h:22), (ptr:7), (key:84)]
- ```
-*/
-
 #include <string>
 #include <utility>
 
@@ -66,7 +39,6 @@ void print(const std::vector<T>& vec) {
         std::cout << "[]";
         return;
     }
-    
     std::cout << "[";
     printSingleElement(*vec.begin());
     for (auto it = vec.begin() + 1; it != vec.end(); ++it) {
