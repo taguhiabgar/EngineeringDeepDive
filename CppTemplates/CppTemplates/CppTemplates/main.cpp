f4768ba034a7582ef592c56d7e445a407a59750d
Taguhi Abgar <taguhiabgar@gmail.com>
2025-03-21 22:45:42 +0400

    C++ Templates

--- CppTemplates/CppTemplates/CppTemplates/main.cpp
+++ CppTemplates/CppTemplates/CppTemplates/main.cpp
@@ -9,11 +9,14 @@
 #include "StackTemplate.h"
 #include "Homework1.h"
 #include "Classwork3.h"
+#include "Classwork4.h"
+#include "Homework4.h"
 
 int main(int argc, const char * argv[]) {
     //Homework1_DriverProgram();
-    Classwork3_DriverProgram();
-    
+    //Classwork3_DriverProgram();
+    //Classwork4_DriverProgram();
+    //Homework4_DriverProgram();
     
     return 0;
 }
